52b9cb8315cce49811dc66ab3d1994167176c0f1
Pavel Nikolaev <altertumlicher@gmail.com>
2025-04-01 17:49:15 +0500

    initial commit

---
+++ DZ7_zadacha1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+int function(std::string str, int forbidden_length)
+{
+	int length = str.length();
+
+	if (length != forbidden_length)
+	{
+		std::cout << "Длина слова " << '"' << str << '"' << " равна " << length << std::endl;
+
+		std::cout << "Введите запретную длину: ";
+		std::cin >> forbidden_length;
+
+		std::cout << "Введите слово: ";
+		std::cin >> str;
+	}
+	else
+	{
+		int bad_length = length;
+
+		try
+		{
+			throw bad_length;
+		}
+		catch (int length)
+		{
+			std::cout << "Вы ввели слово запретной длины! До свидания!" << std::endl;
+			return 0;
+		}
+
+	}
+
+	function(str, forbidden_length);
+
+	return length;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string str{};
+	int forbidden_length{};
+
+	std::cout << "Введите запретную длину: ";
+	std::cin >> forbidden_length;
+
+	std::cout << "Введите слово: ";
+	std::cin >> str;
+
+	function(str, forbidden_length);
+
+	return 0;
+}
